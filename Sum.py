3cc44ec3716a345eaf4008aabcb001fe1cd29eb7
MeherSarana <mehersarana@gmail.com>
2022-10-23 23:15:12 +0530

    Added Two Programs in Python

---
+++ Sum.py
@@ -0,0 +1,12 @@
+import math
+import os
+
+a = int(input("enter 1st number"))
+b = int(input("enter 2nd number"))
+c = int(input("enter 3rd number"))
+
+if (0>=a,b,c<=20): 
+ if(a+b==c or b+c==a or c+a==b):
+    print("yes")
+ else:
+     print("no")
